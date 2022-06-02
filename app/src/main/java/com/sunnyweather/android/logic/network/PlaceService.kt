03620dda95af10267c00281c59480ec636e0a4a2
OXQ <1327635642@qq.com>
2022-06-02 18:06:31 +0800

    预备图片资源

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -0,0 +1,16 @@
+package com.sunnyweather.android.logic.network
+
+import com.sunnyweather.android.SunnyWeatherApplication
+import com.sunnyweather.android.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by oxq on 2022/6/2.
+ */
+interface PlaceService {
+
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+}
